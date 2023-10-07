82451621006a1d96f393da403e47565020517cc7
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-08 00:47:06 +0200

    ?

--- Project.java
+++ Project.java
@@ -1,27 +1,27 @@
+import java.awt.*;
+
 
 public class Project{
 	
 	public static void main(String[] args){ 
 	
-	System.out.println("?");
-	}
-}
-/*
-import javax.swing.JFrame;
-
-public class MyViewer
-{
-   public static void main(String[] args)
-   {
-      JFrame frame = new JFrame();
+	win = new Window();
+	win.show();
+	
+	/*  *** JFrame 
+	import javax.swing.JFrame;	
+	  JFrame frame = new JFrame();
       frame.setSize(300, 400); // Change width and height as needed
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      MyComponent component = new MyComponent();
-      frame.add(component);
+      //MyComponent component = new MyComponent();
+      //frame.add(component);
       frame.setVisible(true);
-   }
-}
-
+	}
+	*/
+	
+	}
+} 
+/*
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
