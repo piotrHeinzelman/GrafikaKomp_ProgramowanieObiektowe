ee751e0711253ed761122dbf64eda73d116076f1
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-08 00:25:50 +0200

    start

---
+++ Project.java
@@ -0,0 +1,37 @@
+
+public class Project{
+	
+	public static void main(String[] args){ 
+	
+	System.out.println("?");
+	}
+}
+/*
+import javax.swing.JFrame;
+
+public class MyViewer
+{
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+      frame.setSize(300, 400); // Change width and height as needed
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      MyComponent component = new MyComponent();
+      frame.add(component);
+      frame.setVisible(true);
+   }
+}
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+public class MyComponent extends JComponent
+{
+   public void paintComponent(Graphics g)
+   {      
+      Graphics2D g2 = (Graphics2D) g;
+      // Your drawing instructions go here
+   }
+}
+*/
---
+++ build_and_run.bat
@@ -0,0 +1,3 @@
+C:\opt\jdk-19\bin\javac.exe Project.java
+C:\opt\jdk-19\bin\java.exe Project
+pause
