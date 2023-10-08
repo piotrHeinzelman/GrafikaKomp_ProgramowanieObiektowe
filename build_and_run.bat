60e9a4b348d7d0d9e210eb71bb019272ad6ae09b
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-08 23:59:03 +0200

    ?

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,8 +1,9 @@
 G:
 cd "G:\JavaDraw"
 
-javac.exe pl\heinzelman\javaDraw\JavaDraw.java -d out
-java.exe out pl.heinzelman.javaDraw.JavaDraw dataWykres.txt
+C:\opt\jdk-19\bin\javac.exe pl\heinzelman\javaDraw\JavaDraw.java -d out
+cd out
+C:\opt\jdk-19\bin\java.exe pl.heinzelman.javaDraw.JavaDraw ..\dataWykres.txt
 pause
 
  
--- out/production/myProject/build_and_run.bat
+++
@@ -1,8 +0,0 @@
-G:
-cd "G:\JavaDraw"
-
-javac.exe pl\heinzelman\javaDraw\JavaDraw.java -d out
-java.exe out pl.heinzelman.javaDraw.JavaDraw dataWykres.txt
-pause
-
- 
