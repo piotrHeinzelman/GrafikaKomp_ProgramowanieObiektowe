2f77fff74427739ea0deec1dacc064718c7a128a
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-08 21:47:28 +0200

    JFrame

--- build_and_run.bat
+++ build_and_run.bat
@@ -3,11 +3,9 @@ cd "C:\Users\John\Desktop\Grafika komputerowa i wizualizacja (2023Z) OK Projekt\
 
 del Project.class
 
-REM C:\opt\jdk-19\bin\javac.exe Project.java
-REM C:\opt\jdk-19\bin\java.exe Project
 
 C:\opt\jdk-19\bin\javac.exe Project.java
-C:\opt\jdk-19\bin\java.exe Project
+C:\opt\jdk-19\bin\java.exe Project dataWykres.txt
 pause
 
  
