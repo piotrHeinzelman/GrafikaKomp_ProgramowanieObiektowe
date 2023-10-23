8068ffd2cd6e7f5cbbb540cf44c8f69134a60b1f
Piotr Heinzelman <piotr.heinzelman@vp.pl>
2023-10-23 09:22:44 +0200

    JAR

---
+++ createJar.bat
@@ -0,0 +1,13 @@
+cd out
+C:\opt\jdk-19\bin\jar.exe cfmv G:\JavaDraw\JavaDraw.jar ..\manifest pl\heinzelman\javaDraw\JavaDraw.class 
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\actions
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\controller
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\model
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\strategy
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\tools
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\treeBSP
+C:\opt\jdk-19\bin\jar.exe uvf  G:\JavaDraw\JavaDraw.jar pl\heinzelman\javaDraw\view
+
+C:\opt\jdk-19\bin\java.exe -jar   G:\JavaDraw\JavaDraw.jar 
+
+pause
---
+++ runme.bat
@@ -0,0 +1,2 @@
+C:\opt\jdk-19\bin\java.exe -jar JavaDraw.jar
+pause
